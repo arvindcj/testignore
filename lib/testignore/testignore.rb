ed758752af81a53ee05833de33bff48df25f26ae
Arvind CJ <code@arvindcj.com>
2015-04-21 17:10:22 +1000

    + rspec fn

--- lib/testignore/testignore.rb
+++ lib/testignore/testignore.rb
@@ -1,17 +1,22 @@
 module TestIgnore
   class File
-    def initialize file
+    def initialize file='.testignore'
       @testignore_file = file
+      @ignore_tags = []
+      @rspec_ignore_tags_list = ''
     end
 
     def read_file
-      file = open(@testignore_file, "w")
-      file.
-        file.close
+      open(@testignore_file, "rb").each_line do |line|
+        @ignore_tags << line.chomp if !line.match /#/
+      end
     end
 
     def rspec_ignore_tags
-      "--tags "
+      @ignore_tags.each do |tag|
+        @rspec_ignore_tags_list << " --tags ~#{tag}" 
+      end
+      @rspec_ignore_tags_list
     end
 
     def  cucumber_ignore_tags
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,5 @@
 require 'testignore'
+
+q = TestIgnore::File.new
+q.read_file
+puts q.rspec_ignore_tags
